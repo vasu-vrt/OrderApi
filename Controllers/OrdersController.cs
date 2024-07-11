1b6b64ff487449d402d0553620b9cd6173500025
ASD <vasuvdeshpande@gmail.com>
2024-07-11 08:57:44 +0530

    Add project files.

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderApi.Model;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private static List<Order> Orders = new List<Order>();
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(Orders);
+        }
+
+        [HttpPost]
+        public IActionResult AddOrder([FromBody] Order order)
+        {
+            order.Id = Orders.Count + 1;
+            Orders.Add(order);
+            return Ok(order);
+        }
+    }
+
+}
---
+++ Model/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderApi.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
